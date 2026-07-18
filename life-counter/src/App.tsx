7e7688cadb67080e1c4e3f89d77b667a64c4a6b0
Travis <travism.github@gmail.com>
2026-07-18 17:22:20 -0400

    added icons for rolling dice, changeing the amount of users, restarting the game, and changing everyones starting health

--- life-counter/src/App.tsx
+++ life-counter/src/App.tsx
@@ -1,8 +1,20 @@
 import { Counter } from "./components/Counter";
+import { Dices, User, RefreshCw, Heart } from "lucide-react";
 
 export default function () {
   return (
     <div>
+      <div className="settings-bar">
+        <Dices />
+        <User />
+        <RefreshCw />
+        <Heart />
+      </div>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
       <Counter />
     </div>
   );
--- life-counter/src/components/Counter.tsx
+++ life-counter/src/components/Counter.tsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 export function Counter() {
   const [count, setCount] = useState(20);
   return (
-    <div>
+    <div
+      style={{
+        border: "solid",
+        width: "fit-content",
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
       <button onClick={() => setCount(count - 1)}>- 1</button>
       <div>{count}</div>
       <button onClick={() => setCount(count + 1)}>+ 1</button>
